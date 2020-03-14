9f5f33cdd10d65fb487d27c960d8724acc66cd75
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2020-03-14 09:22:42 -0400

    created dummy model
    added dummy model with predict, no train method has been added yet

---
+++ suicide_squad/model.py
@@ -0,0 +1,20 @@
+from transformers import BertTokenizer, BertForQuestionAnswering,BertConfig
+import torch
+
+
+class QuestionAnswering():
+    def __init__(self,model_configs):
+        self.model_configs=model_configs
+        self.model = BertForQuestionAnswering.from_pretrained(self.model_configs['pretrained_model_name'],
+                                                             cache_dir=self.model_configs['cache_dir'])
+        
+        self.tokenizer = BertTokenizer.from_pretrained(self.model_configs['tokenizer_name'])
+    
+    def predict(self,question,text):
+        input_ids = self.tokenizer.encode(question, text)
+        token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
+        start_scores, end_scores = self.model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
+
+        all_tokens = self.tokenizer.convert_ids_to_tokens(input_ids)
+        answer = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1])
+        return(answer)
---
+++ suicide_squad/model_configs.py
@@ -0,0 +1,4 @@
+model_configs={'pretrained_model_name':'bert-large-uncased-whole-word-masking-finetuned-squad',
+               'tokenizer_name':'bert-base-uncased',
+               'cache_dir':'./cache_dir'
+              }
