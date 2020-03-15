84b119ad3bfe3c8caa67d07edb6a7bd553b4f0bc
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2020-03-15 12:46:28 -0400

    added new funtions
    New functions added to datautils

--- suicide_squad/DataUtils.py
+++ suicide_squad/DataUtils.py
@@ -20,6 +20,19 @@ sys.path.insert(0, cwd)
 download_folder=os.path.join(cwd,'download')
 if(not os.path.exists(download_folder)):
     os.mkdir(download_folder)
+    
+data_folder=os.path.join(cwd,'data')
+if(not os.path.exists(data_folder)):
+    os.mkdir(data_folder)
+    
+original_data_folder=os.path.join(data_folder,'original')
+if(not os.path.exists(original_data_folder)):
+    os.mkdir(original_data_folder)
+    
+processed_data_folder=os.path.join(data_folder,'processed')
+if(not os.path.exists(processed_data_folder)):
+    os.mkdir(processed_data_folder)
+
 # else:
 #     print('Download folder exists', download_folder)
 
@@ -27,19 +40,21 @@ if(not os.path.exists(download_folder)):
 def check_file(filename, location=cwd):
     return (os.path.exists(os.path.join(location, filename)),os.path.join(location, filename))
 
-def download_files(files, download_folder):
+def download_files(files, download_folder,return_paths=True):
     #Author :Rouzbeh Afrasiabi :https://github.com/rouzbeh-afrasiabi
     """
     """
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    
+    file_paths=[]
     for file in files:
         [[_, location], ] = file.items()
         file_name = os.path.basename(location)
+        file_path=os.path.join(download_folder,file_name)
         (exists, _) = check_file(file_name, download_folder)
         if exists:
-            print (file_name, ' Already Exists.')
+            file_paths.append(file_path)
+#             print (file_name, ' Already Exists.')
             pass
         else:
             print ('*** Downloading : ', file_name)
@@ -50,10 +65,13 @@ def download_files(files, download_folder):
                 with open(os.path.join(download_folder, file_name), 'wb'
                           ) as f:
                     shutil.copyfileobj(r.raw, f)
+                file_paths.append(file_path)
             except:
                 raise Exception('Failed')
+    if(return_paths):
+        return(file_paths)
                 
-def load_pandas_df(local_cache_path="download", squad_version="v1.1", file_split="train"):
+def load_pandas_df(file_path, download_folder):
     #https://github.com/microsoft/nlp-recipes/blob/master/utils_nlp/dataset/squad.py
     # Copyright (c) Microsoft Corporation. All rights reserved.
     # Licensed under the MIT License.
@@ -70,12 +88,12 @@ def load_pandas_df(local_cache_path="download", squad_version="v1.1", file_split
             Defaults to "train".
     """
 
-    if file_split not in ["train", "dev"]:
-        raise ValueError("file_split should be either train or dev")
+#     if file_split not in ["train", "dev"]:
+#         raise ValueError("file_split should be either train or dev")
     
-    file_name=os.path.join(file_split+'-'+squad_version+'.json')
+#     file_name=os.path.join(file_split+'-'+squad_version+'.json')
 
-    file_path = os.path.join(local_cache_path, file_name)
+#     file_path = os.path.join(local_cache_path, file_name)
 
     with open(file_path, "r", encoding="utf-8") as reader:
         input_data = json.load(reader)["data"]
@@ -97,10 +115,10 @@ def load_pandas_df(local_cache_path="download", squad_version="v1.1", file_split
                 answer_offset = None
                 is_impossible = False
 
-                if squad_version == "v2.0":
+                if "v2.0" in file_path:
                     is_impossible = qa["is_impossible"]
 
-                if file_split == "train":
+                if "train" in file_path:
                     if (len(qa["answers"]) != 1) and (not is_impossible):
                         raise ValueError(
                             "For training, each question should have exactly 1 answer."
@@ -138,6 +156,33 @@ def load_pandas_df(local_cache_path="download", squad_version="v1.1", file_split
             "is_impossible": is_impossible_list,
         }
     )
-    output_df.name=file_name
     return output_df
 
+def squad_to_csv(files, download_folder,return_paths=True):
+    print('Downloading Files')
+    file_paths=download_files(files, download_folder)
+    print('Converting Files to CSV')
+    new_file_paths=[]
+    for file_path in file_paths:
+        file_name = os.path.basename(file_path).replace('.json','.csv')
+        new_path=os.path.join(original_data_folder,file_name)
+        if(not  os.path.exists(new_path)):
+            temp=load_pandas_df(file_path, download_folder)
+            temp.to_csv(new_path,encoding='utf8')
+        new_file_paths.append(new_path)
+    print('Finished Downloading and Converting to CSV')
+    if(return_paths):
+        return(new_file_paths)
+
+def squad_to_df(files, download_folder):
+    print('Downloading Files')
+    file_paths=squad_to_csv(files, download_folder)
+    print('Converting Files to DataFrame')
+    dataframes={}
+    for file_path in file_paths:
+        file_name = os.path.basename(file_path).replace('.json','.csv')
+        new_path=os.path.join(original_data_folder,file_name)
+        temp=pd.read_csv(new_path,index_col=[0])
+        dataframes[file_name.replace('.csv','')]=temp
+    print('Finished Downloading and Converting to DataFrame')
+    return(dataframes)
